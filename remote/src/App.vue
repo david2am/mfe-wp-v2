c9568c4470662dabec919f381b0a5b3482a53dae
ddevarg <david.devargas@morningstar.com>
2023-03-21 08:59:28 -0500

    init setup

---
+++ remote/src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container">
+    <div>Name: remote</div>
+    <div>Framework: vue2</div>
+    <div>Language: JavaScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+</template>
